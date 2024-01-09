8c7f44cd73a2fab9b85513bf1fba31d5cd975178
Eric Winebrenner <ejwinebrenner@gmail.com>
2024-01-09 18:09:28 -0500

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
-
+import Employees from './components/employeeinfo/Employees';
 
 function App() {
   return (
     <div className="App">
       <h1>Components & Props</h1>
+      <Employees />
     </div>
   );
 }
