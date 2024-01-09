78c1ffe5d6d92417a4ae464e8ad7344506616188
Eric Winebrenner <ejwinebrenner@gmail.com>
2024-01-09 16:27:58 -0500

    updated notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css';
-import AboutMe from './components/aboutme/AboutMe';
-import Footer from './components/footer/Footer';
-import Header from './components/header/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <AboutMe />
-      <Footer />
+      <h1>Components & Props</h1>
     </div>
   );
 }
